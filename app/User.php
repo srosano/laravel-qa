f339329ad3cb69bf72d69959c7def3a365dc1deb
scott rosano <scott.rosano@gmail.com>
2022-02-11 14:43:25 +1100

    Added email verification and tested with mailtrap.io

--- app/User.php
+++ app/User.php
@@ -70,10 +70,10 @@ class User extends Authenticatable implements MustVerifyEmail
      * The attributes that should be cast to native types.
      *
      * @var array
-     *
-     *protected $casts = [
-     *    'email_verified_at' => 'datetime',
-     *];*/
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 
     public function questions()
     {
