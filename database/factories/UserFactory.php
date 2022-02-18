fc29b85212b61927e76213505a3c6ce2eb7234ba
scott rosano <scott.rosano@gmail.com>
2022-02-18 16:03:40 +1100

    Dealing with composer dep laravel-ide-helper

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -4,7 +4,7 @@
 
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
-
+use App\User;
 /*
 |--------------------------------------------------------------------------
 | Model Factories
@@ -16,7 +16,7 @@ use Illuminate\Support\Str;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
